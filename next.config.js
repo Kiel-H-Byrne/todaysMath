629605f44979e0b3216426752d3fcc14208b0f17
Kiel Byrne <kiel.byrne@gmail.com>
2022-11-13 22:14:01 -0500

    config update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  swcMinify: true,
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+    swcMinify: true,
+  }
+  return nextConfig
 }
